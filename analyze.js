70fe06f1e6542717a0c4dfab6dda2f7cf804ebd9
Filip Troníček <filip.tronicek@seznam.cz>
2020-03-07 17:13:48 +0100

    Ejs rendering and encoding HTML

--- analyze.js
+++ analyze.js
@@ -4,6 +4,13 @@ const {
 } = require("@azure/ai-text-analytics");
 const credentials = require("./creds.js");
 
+function encodeHTML(text) {
+  text = text.replace("<", "&lt;");
+  text = text.replace(">", "&gt;");
+  return text;
+}
+
+
 function GetAnalyzed(txt) {
   "use strict";
   const textAnalyticsClient = new TextAnalyticsClient(
@@ -20,7 +27,7 @@ function GetAnalyzed(txt) {
       document.entities.forEach(entity => {
         console.log(`URL: ${entity.url}`);
         entity.matches.forEach(match => {
-          console.log(txt.substr(match.offset, match.length + match.offset));
+          return txt.substr(match.offset, match.length + match.offset);
         });
       });
     });
--- index.js
+++ index.js
@@ -2,18 +2,23 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+
 app.use(express.static('views'));
+/*
 app.use(
     cors({
       origin: "https://related-pics.now.sh"
     })
   );
+*/
 app.get("/", function(req, res) {
-    res.render("/views/index.html");
+    res.render("index");
 });
 app.get("/analyze", function(req, res) {
     const analyze = require("./analyze");
-    analyze.do("This is a Hello World template for GCP, Azure, Watson and AWS");
+    res.render("analyze", {returnStuff: analyze.do("This is a Hello World template for GCP, Azure, Watson and AWS")});
 });
 app.listen(5000, function() {
     console.log(`Related Pics listening on port ${5000}!`);
