5419726949324da61e7ecdb10512b61250b2245f
Filip Troníček <filip.tronicek@seznam.cz>
2020-03-11 19:09:46 +0100

    Return the object to the HTML

--- index.js
+++ index.js
@@ -18,7 +18,49 @@ app.get("/", function(req, res) {
 });
 app.get("/analyze", function(req, res) {
     const analyze = require("./analyze");
-    res.render("analyze", {returnStuff: analyze.do("This is a Hello World template for GCP, Azure, Watson and AWS")});
+
+    const {
+      TextAnalyticsClient,
+      TextAnalyticsApiKeyCredential
+    } = require("@azure/ai-text-analytics");
+    const credentials = require("./creds.js");
+    
+    function encodeHTML(text) {
+      text = text.replace("<", "&lt;");
+      text = text.replace(">", "&gt;");
+      return text;
+    }    
+    function GetAnalyzed(txt) {
+      "use strict";
+      const textAnalyticsClient = new TextAnalyticsClient(
+        credentials.creds.endpoint,
+        new TextAnalyticsApiKeyCredential(credentials.creds.key)
+      );
+      async function linkedEntityRecognition(client) {
+        const linkedEntityInput = [txt];
+        const entityResults = await client.recognizeLinkedEntities(
+          linkedEntityInput
+        );
+    
+        entityResults.forEach(document => {
+          document.entities.forEach(entity => {
+            console.log(`URL: ${entity.url}`);
+            entity.matches.forEach(match => {
+              return txt.substr(match.offset, match.length + match.offset);
+            });
+          });
+    
+        });
+        res.render("analyze", {returnStuff: entityResults});
+
+      }
+
+      linkedEntityRecognition(textAnalyticsClient);
+
+    }
+    const reply = GetAnalyzed("This is a Hello World template for GCP, Azure, Watson and AWS");
+
+   
 });
 app.listen(5000, function() {
     console.log(`Related Pics listening on port ${5000}!`);
