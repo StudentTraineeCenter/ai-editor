1d5a6cdeface3695d347a6b311f63100baae4f07
Filip Troníček <filip.tronicek@seznam.cz>
2020-03-16 19:24:21 +0100

    Spill out some text

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static('views'));
+app.use(express.static("views"));
 /*
 app.use(
     cors({
@@ -14,54 +14,52 @@ app.use(
   );
 */
 app.get("/", function(req, res) {
-    res.render("index");
+  res.render("index");
 });
 app.get("/analyze", function(req, res) {
-    const analyze = require("./analyze");
+  const analyze = require("./analyze");
 
-    const {
-      TextAnalyticsClient,
-      TextAnalyticsApiKeyCredential
-    } = require("@azure/ai-text-analytics");
-    const credentials = require("./creds.js");
-    
-    function encodeHTML(text) {
-      text = text.replace("<", "&lt;");
-      text = text.replace(">", "&gt;");
-      return text;
-    }    
-    function GetAnalyzed(txt) {
-      "use strict";
-      const textAnalyticsClient = new TextAnalyticsClient(
-        credentials.creds.endpoint,
-        new TextAnalyticsApiKeyCredential(credentials.creds.key)
+  const {
+    TextAnalyticsClient,
+    TextAnalyticsApiKeyCredential
+  } = require("@azure/ai-text-analytics");
+  const credentials = require("./creds.js");
+
+  function encodeHTML(text) {
+    text = text.replace("<", "&lt;");
+    text = text.replace(">", "&gt;");
+    return text;
+  }
+  function GetAnalyzed(txt) {
+    "use strict";
+    const textAnalyticsClient = new TextAnalyticsClient(
+      credentials.creds.endpoint,
+      new TextAnalyticsApiKeyCredential(credentials.creds.key)
+    );
+    async function linkedEntityRecognition(client) {
+      const linkedEntityInput = [txt];
+      const entityResults = await client.recognizeLinkedEntities(
+        linkedEntityInput
       );
-      async function linkedEntityRecognition(client) {
-        const linkedEntityInput = [txt];
-        const entityResults = await client.recognizeLinkedEntities(
-          linkedEntityInput
-        );
-    
-        entityResults.forEach(document => {
-          document.entities.forEach(entity => {
-            console.log(`URL: ${entity.url}`);
-            entity.matches.forEach(match => {
-              return txt.substr(match.offset, match.length + match.offset);
-            });
+
+      entityResults.forEach(document => {
+        document.entities.forEach(entity => {
+          console.log(`URL: ${entity.url}`);
+          entity.matches.forEach(match => {
+            return txt.substr(match.offset, match.length + match.offset);
           });
-    
         });
-        res.render("analyze", {returnStuff: entityResults});
-
-      }
-
-      linkedEntityRecognition(textAnalyticsClient);
-
+      });
+      console.log(entityResults[0].entities);
+      res.render("analyze", { returnStuff: entityResults[0].entities[1].url });
     }
-    const reply = GetAnalyzed("This is a Hello World template for GCP, Azure, Watson and AWS");
 
-   
+    linkedEntityRecognition(textAnalyticsClient);
+  }
+  const reply = GetAnalyzed(
+    "This is a Hello World template for GCP, Azure, Watson and AWS"
+  );
 });
 app.listen(5000, function() {
-    console.log(`Related Pics listening on port ${5000}!`);
-  });
+  console.log(`Related Pics listening on port ${5000}!`);
+});
