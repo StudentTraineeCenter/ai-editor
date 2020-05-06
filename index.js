db7acb9f51fc25ac14060e513977111d7fd30a8a
Filip Troníček <filip.tronicek@seznam.cz>
2020-05-06 17:47:43 +0200

    Lmao

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.get("/analyze", function (req, res) {
         });
       });
       res.render("analyze", {
-        returnUrls: entityResults[0].entities,
+        returnStuff: entityResults[0].entities,
         returnText: boldenKeyphrases(
           encodeHTML(txt),
           keyPhraseResult[0].keyPhrases
