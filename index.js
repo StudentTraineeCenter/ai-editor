c77359441afffab9f4c7a4e77400e0c2c7f5413f
Filip Troníček <filip.tronicek@seznam.cz>
2020-05-06 18:24:57 +0200

    Fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
 
 const {
   TextAnalyticsClient,
-  TextAnalyticsApiKeyCredential,
+  AzureKeyCredential,
 } = require("@azure/ai-text-analytics");
 
 app.set("view engine", "ejs");
@@ -19,7 +19,6 @@ app.get("/analyze", function (req, res) {
 });
 
 app.get("/", function (req, res) {
-
   let key, endpoint;
 
   if (typeof process.env.endpoint != "undefined") {
@@ -77,7 +76,8 @@ app.get("/", function (req, res) {
     linkedEntityRecognition(textAnalyticsClient);
   }
   GetAnalyzed(
-    req.query.txt || "We went to Contoso Steakhouse located at midtown NYC last week for a dinner party, and we adore the spot! They provide marvelous food and they have a great menu. The chief cook happens to be the owner (I think his name is John Doe) and he is super nice, coming out of the kitchen and greeted us all. We enjoyed very much dining in the place! The Sirloin steak I ordered was tender and juicy, and the place was impeccably clean. You can even pre-order from their online menu at www.contososteakhouse.com, call 312-555-0176 or send email to order@contososteakhouse.com! The only complaint I have is the food didn't come fast enough. Overall I highly recommend it!"
+    req.query.txt ||
+      "We went to Contoso Steakhouse located at midtown NYC last week for a dinner party, and we adore the spot! They provide marvelous food and they have a great menu. The chief cook happens to be the owner (I think his name is John Doe) and he is super nice, coming out of the kitchen and greeted us all. We enjoyed very much dining in the place! The Sirloin steak I ordered was tender and juicy, and the place was impeccably clean. You can even pre-order from their online menu at www.contososteakhouse.com, call 312-555-0176 or send email to order@contososteakhouse.com! The only complaint I have is the food didn't come fast enough. Overall I highly recommend it!"
   );
 });
 app.listen(port, function () {
